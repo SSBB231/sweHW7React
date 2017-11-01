2b9e95086732fc9729228a96efd357636219f196
Benjamin <bengeyer@live.com>
2017-11-01 00:52:03 -0400

    allusers table not showing up

--- src/App.js
+++ src/App.js
@@ -28,61 +28,39 @@ class App extends Component
             </div>
         );
     }
-
 }
 
-<<<<<<< HEAD
 class AllUsers extends Component {
-    render() {
-        return (
-            <table>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td>Knocky</td>
-                    <td>Flor</td>
-                    <td>Ella</td>
-                    <td>Juan</td>
-                </tr>
-                <tr>
-                    <td>Breed</td>
-                    <td>Jack Russell</td>
-                    <td>Poodle</td>
-                    <td>Streetdog</td>
-                    <td>Cocker Spaniel</td>
-                </tr>
-                <tr>
-                    <td>Age</td>
-                    <td>16</td>
-                    <td>9</td>
-                    <td>10</td>
-                    <td>5</td>
-                </tr>
-                <tr>
-                    <td>Owner</td>
-                    <td>Mother-in-law</td>
-                    <td>Me</td>
-                    <td>Me</td>
-                    <td>Sister-in-law</td>
-                </tr>
-                <tr>
-                    <td>Eating Habits</td>
-                    <td>Eats everyone's leftovers</td>
-                    <td>Nibbles at food</td>
-                    <td>Hearty eater</td>
-                    <td>Will eat till he explodes</td>
-                </tr>
-            </table>
-        );
-=======
-class AllUsers extends Component
-{
-    render()
-    {
-        return (<div></div>);
->>>>>>> 30a13e83c110f78eb49835139c792be1b9776823
+    constructor(props){
+        super(props);
+        this.state = {size: 3}
     }
+    render(){
+            let rows = [];
+            for (var i = 0; i < this.state.size; i++){
+                let rowID = `row${i}`
+                let cell = []
+                for (var idx = 0; idx < this.state.size; idx++){
+                    let cellID = `cell${i}-${idx}`
+                    cell.push(<td key={cellID} id={cellID}></td>)
+                }
+                rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+            }
+            return(
+                <div className="container">
+                    <div className="row">
+                        <div className="col s12 board">
+                            <table id="simple-board">
+                                <tbody>
+                                {rows}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
 }
-
 class HomeScreen extends Component
 {
     render()
